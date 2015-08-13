9caa8d01fb62bbd2ff223eee86a27f68f7ce6621
peruvian <peruvian@192.168.1.38>
2015-08-13 16:59:31 -0300

    19  Grima

--- protected/components/numero_venta.php
+++ protected/components/numero_venta.php
@@ -18,15 +18,21 @@ function conexion() {
 function getOrden() {
 
     //mysqli_select_db('pruebas', $link);
-    
+//    $sql = "SELECT Id,NumCompra from compra ORDER by Id ASC";
     $sql = "SELECT Id,NumCompra from compra ORDER by Id ASC";
     $resultado = mysqli_query(conexion(), $sql);
     //$resultado = mysql_query($sql) or die( "error en $sql, " . mysql_error() );
-    while($row = mysqli_fetch_array($resultado)){
-        $orden = $row['NumCompra'];
-    }
     
-
+    while($row = mysqli_fetch_array($resultado)){
+        
+        $orden = $row['Id'];
+    }
+    switch ($orden){
+        case $orden = '':
+            $orden = 1;
+        case $orden =! 0:
+            $orden+=1;
+    }
     echo (int) $orden+=1;
 }
 
