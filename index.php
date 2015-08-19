0e888c9dda1529c01dcd8a269652e2b959d50dd6
peruvian <peruvian@192.168.1.33>
2015-08-19 09:38:02 -0300

    24  Grima

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'../../../yii/framework/yii.php';
-
 //$yii=dirname(__FILE__).'../../yii/framework/yii.php';
+
+$yii=dirname(__FILE__).'../../../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
